61ae01c43bab53e8fd83bb2298c1c3ba249b96a9
arthar <929838354@qq.com>
2016-12-15 10:46:36 +0800

    dev anim

--- app/src/main/java/com/studyjams/mdvideo/MainFrame/WelcomeActivity.java
+++ app/src/main/java/com/studyjams/mdvideo/MainFrame/WelcomeActivity.java
@@ -1,10 +1,10 @@
 package com.studyjams.mdvideo.MainFrame;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
